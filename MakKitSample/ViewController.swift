57ac9f739c2319c20972840796d35b7a32522d37
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2016-11-10 22:56:43 -0800

    Added centering to user location and following user as he moves. End of sample app

--- MakKitSample/ViewController.swift
+++ MakKitSample/ViewController.swift
@@ -9,19 +9,33 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
     @IBAction func zoomIn(_ sender: Any) {
+        
+        let userLocation = mapView.userLocation
+        
+        let region = MKCoordinateRegionMakeWithDistance((userLocation.location?.coordinate)!, 2000, 2000)
+        
+        mapView.setRegion(region, animated: true)
+        
     }
     
     @IBAction func changeMapType(_ sender: Any) {
+        
+        if mapView.mapType == MKMapType.standard {
+            mapView.mapType = MKMapType.satellite
+        } else {
+            mapView.mapType = MKMapType.standard
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.showsUserLocation = true
+        mapView.delegate = self
         
         
         
@@ -32,6 +46,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    public func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        mapView.centerCoordinate = (userLocation.location?.coordinate)!
     }
 
 
